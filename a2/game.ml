976ae1deee5f2f3aa96c8576b2f8d5e37f73ccc7
Pujaa Rajan <pujaa.rajan@gmail.com>
2015-09-22 15:38:37 -0400

    finished record types

--- a2/game.ml
+++ a2/game.ml
@@ -1,22 +1,22 @@
 open Yojson.Basic.Util ;;
 exception Rooms_parse_error;;
 exception Items_parse_error;;
-
-
-type room = {
-  id : string;
-  description : string;
-  items : string list;
-  points : int;
-  treasure : string list;
-  exits : (string * room) list;
-  }
+exception Start_room_parse_error
 
 type item = {
   id : string;
   description : string;
   points : int;
 }
+
+type room = {
+  id : string;
+  description : string;
+  items : item list;
+  points : int;
+  treasure : item list;
+  exits : (string * room) list;
+  }
 
 type state = {
   room : string;
@@ -41,10 +41,20 @@ let extract_rooms (t : Yojson.Basic.json) : room list =
     in List.map (fun ( r : Yojson.Basic.json) -> {
       id = r |> member "id" |> to_string;
       description = r |> member "description" |> to_string;
-      items = [r] |> filter_member "items" |> flatten |> filter_string;
+      items =
+        List.map (fun ( s : Yojson.Basic.json) -> {
+            id = s |> member "id" |> to_string;
+            description = s |> member "description" |> to_string;
+            points = s |> member "points" |> to_int;
+            }) (r |> member "items" |> to_list);
       points = r |> member "points" |> to_int;
-      treasure = [r] |> filter_member "treasure" |> flatten |> filter_string;
-      exits = []
+      treasure =
+          List.map (fun ( s : Yojson.Basic.json) -> {
+            id = s |> member "id" |> to_string;
+            description = s |> member "description" |> to_string;
+            points = s |> member "points" |> to_int;
+            }) (r |> member "treasure" |> to_list);
+      exits = [];
     }) raw
   with _ -> raise Rooms_parse_error
 
@@ -53,47 +63,29 @@ let update_rooms (t : Yojson.Basic.json) (r : room list) : room list=
     |> filter_member "rooms"
     |> flatten
   in let direction_room = List.map
-        (fun ( s : Yojson.Basic.json) ->
-        (s |> member "direction" |> to_string, s |> member "room" |> to_string))
-        (raw |> filter_member "exits" |> flatten)
+    (fun ( s : Yojson.Basic.json) ->
+    (s |> member "direction" |> to_string, s |> member "room" |> to_string))
+    (raw |> filter_member "exits" |> flatten)
   in let map_room (direction, room_id) =
-        let room = List.find
-          (fun (r: room) -> r.id = room_id) r in
-          (direction, room) in
-  let new_exits: (string* room) list = List.map map_room direction_room in
+    let room = List.find
+      (fun (r: room) -> r.id = room_id) r in
+      (direction, room) in
+  let new_exits: (string * room) list = List.map map_room direction_room in
    List.map (fun room -> {room with exits = new_exits}) r
 
-let extract_items (t : Yojson.Basic.json) : item list =
-  try
-    let raw = [t]
-      |> filter_member "items"
-      |> flatten
-    in List.map (fun ( r : Yojson.Basic.json) -> {
-      id = r |> member "id" |> to_string;
-      description = r |> member "description" |> to_string;
-      points = r |> member "points" |> to_int;
-    }) raw
-  with _ -> raise Items_parse_error
-
-  let update_items (t : Yojson.Basic.json) (r : item list) : room list=
-    let raw = [t]
-      |> filter_member "items"
-      |> flatten
-    in let item_list = List.map
-        (fun ( s : Yojson.Basic.json) ->
-        (s |> member "item" |> to_string, s |> member "room" |> to_string))
-        (raw |> filter_member "exits" |> flatten)
-    in let map_room (direction, room_id) =
-        let room = List.find
-          (fun (r: room) -> r.id = room_id) r in
-          (direction, room) in
-    let new_exits: (string* room) list = List.map map_room direction_room in
-    List.map (fun room -> {room with exits = new_exits}) r
-
-(*let inputs = parse_commands (Array.to_list Sys.argv) in
+let inputs = parse_commands (Array.to_list Sys.argv) in
   let raw_rooms = extract_rooms inputs in
-    update_rooms inputs raw_rooms*)
+    update_rooms inputs raw_rooms
 
-let test = parse_commands (Array.to_list Sys.argv) in
-  raw_items = extract_items test
+let start_room (t : Yojson.Basic.json) (r : room list) : room =
+  let start_room_id =
+    t |> member "start_room" |> to_string
+  in List.find
+  (fun (r: room) -> r.id = start_room_id) r
 
+(*create current game state*)
+let game_state = {
+  room : string;
+  items : string;
+  points : int;
+}

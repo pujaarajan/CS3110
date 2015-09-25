410579dfd5778c21b243dff927816c3ca24a6b59
Pujaa Rajan <pujaa.rajan@gmail.com>
2015-09-24 21:36:05 -0400

    almost done

--- a2/game.ml
+++ a2/game.ml
@@ -56,82 +56,20 @@ type item = {
   item_points : int;
 }
 
-let extract_items (t : Yojson.Basic.json) : (string * item) list =
+let extract_items (t : Yojson.Basic.json) : item list =
   try
     let raw = [t]
       |> filter_member "items"
       |> flatten
-    in List.map (fun ( r : Yojson.Basic.json) -> (r |> member "id" |> to_string, {
+    in List.map (fun ( r : Yojson.Basic.json) -> {
       item_id = r |> member "id" |> to_string;
       item_description = r |> member "description" |> to_string;
       item_points = r |> member "points" |> to_int;
-    })) raw
+    }) raw
   with _ -> raise Extract_items_error
 
 let string_to_item (item_id : bytes) (i : item list) : item =
   List.find(fun (i : item) -> i.item_id = item_id) i
-
-
-(*TAKE*)
-
-(*HELP - WHY DO I NEED AN IN IN ALL 3 OF THESE*)
-let takeitem_updateplayer (player : player) (item_id : string) : player =
-  if List.exists (fun (s: string) -> s = item_id) player.player_items then
-    let () = print_string("You already have that item.\n") in player
-    else
-    { player with player_items = (add_s_to_l player.player_items item_id); }
-
-let takeitem_updaterooms (rooms : room) (item_id : string) : room =
-  if List.exists (fun (s: string) -> s = item_id) rooms.room_items then
-    let () = print_string("You already have that item.\n") in rooms
-    else
-    { room with room_items = List.filter (fun (s: string) -> s <> item_id) rooms.room_items; }
-
-let takeitem_updategame (game_now : game_now) (item_id : string) : game_now =
-  if List.exists (fun (s: string) -> s = item_id) game_now.your_items then
-    let () = print_string("You already have that item.\n") in game_now
-    else
-    let item_point = (string_to_item item_id).item_points in
-    { game_now with your_items = (game_now.your_items@[item_id]); room_points = sum(game_now.your_points@[item_point]) }
-
-(*DROP*)
-
-let dropitem_updateplayer (player : player) (item_id : string) : player =
-  if List.exists (fun (s: string) -> s = item_id) player.player_items then
-    { player with player_items = List.filter (fun (s: string) -> s <> item_id) player.player_items; }
-    in player
-    else
-    let () = print_string("You don't have that item to drop.\n") in player
-
-let dropitem_updaterooms (rooms : room) (item_id : string) : room =
-  if List.exists (fun (s: string) -> s = item_id) rooms.room_items then
-    { room with room_items = List.filter (fun (s: string) -> s <> item_id) rooms.room_items; }
-    else
-    let () = print_string("You don't have that item to drop.\n") in rooms
-
-
-(*MOVE*)
-
-let move (game: GameState.params) (player: Player.params)
-  (input_direction: string) : Player.params * GameState.params =
-  let exits = player.room.exits in
-  let bool_val = List.exists (fun (index: Exit.params) ->
-    index.direction = input_direction) exits in
-  if bool_val then
-    let directed_room_exit =
-      List.find (fun (exit: Exit.params) -> exit.direction = input_direction) exits in
-    let directed_room =
-      GameState.find_room_by_id game directed_room_exit.room in
-    let updated_room = Room.reset_score directed_room in
-    let updated_player = { player with
-    room = updated_room;
-    score = player.score + directed_room.points;
-    } in
-    (updated_player, GameState.update_room game updated_room)
-    else
-      let () = print_string ("You cant walk in that direction\n") in
-      (Player.add_turn player, game)
-
 
 (*PLAYER*)
 
@@ -182,6 +120,71 @@ let game_all (t : Yojson.Basic.json) : game_all = {
     sum(room_points@item_points);
     }
 
+(*Functions*)
+
+(*TAKE*)
+
+let takeitem_updateplayer (player : player) (item_id : string) : player =
+  if List.exists (fun (s: string) -> s = item_id) player.player_items then
+    let () = print_string("You already have that item.\n") in player
+    else
+    { player with player_items = (add_s_to_l player.player_items item_id); }
+
+let takeitem_updaterooms (room_id : string) (item_id : string) (room_list : room list) : room list =
+  let room_record = string_to_room room_id room_list in
+  if List.exists (fun (s: string) -> s = item_id) room_record.room_items then
+    let () = print_string("You already have that item.\n") in room_list
+  else
+    let new_room = { room_record with room_items = List.filter (fun (s: string) -> s <> item_id) room_record.room_items; } in
+    List.map (fun x -> if x.room_id = room_id then new_room else x ) room_list
+
+let takeitem_updategame (game_now : game_now) (item_id : string) (item_list : item list) : game_now =
+  if List.exists (fun (s: string) -> s = item_id) game_now.your_items then
+    let () = print_string("You already have that item.\n") in game_now
+    else
+    { game_now with your_items = (game_now.your_items@[item_id]); }
+
+(*DROP*)
+
+let dropitem_updateplayer (player : player) (item_id : string) : player =
+  if List.exists (fun (s: string) -> s = item_id) player.player_items then
+    { player with player_items = List.filter (fun (s: string) -> s <> item_id) player.player_items; }
+    else
+    let () = print_string("You don't have that item to drop.\n") in player
+
+let dropitem_updaterooms (room_id : string) (item_id : string) (room_list : room list) : room list=
+  let room_record = string_to_room room_id room_list in
+  if List.exists (fun (s: string) -> s <> item_id) room_record.room_items then
+  (*adding item*)
+    let new_room = { room_record with room_items =  add_s_to_l room_record.room_items item_id} in
+    List.map (fun x -> if x.room_id = room_id then new_room else x ) room_list
+    else
+    let () = print_string("That item is already in the room.\n") in room_list
+
+(*MOVE*)
+
+let go_updateplayer (direction: string) (player: player) (room_list : room list) : player =
+  let room_now = string_to_room player.player_room room_list in
+  if List.exists (fun (s: string*string) -> fst s = direction) room_now.room_exits then
+    let direction_room = List.find (fun (s: string*string) -> fst s = direction) room_now.room_exits in
+    { player with player_room = (snd direction_room) ; }
+  else
+  let () = print_string ("You cant walk in that direction\n") in player
+
+let go_updategame (direction:string) (player:player) (game_now:game_now) (room_list : room list) : game_now =
+  let room_now = string_to_room player.player_room room_list in
+  if List.exists (fun (s: string*string) -> fst s = direction) room_now.room_exits then
+    let direction_room = List.find (fun (s: string*string) -> fst s = direction) room_now.room_exits in
+    if List.exists (fun (s: string) -> s <> (snd direction_room)) game_now.your_rooms then
+      { game_now with your_rooms = (game_now.your_rooms@[(snd direction_room)]); }
+    else
+      game_now
+  else
+  game_now
+
+(*add room to rooms list in game now*)
+
+
 (*Test functions*)
 
 (*let file_input = String.lowercase(String.trim(Sys.argv.(1))) in
@@ -197,7 +200,7 @@ in Yojson.Basic.from_file(second_input)
 
 let inputs = parse_commands (Array.to_list Sys.argv)
 let repl_rooms = extract_rooms inputs
-let items = extract_items inputs
+let repl_items = extract_items inputs
 let repl_player = player(inputs)
 let repl_game_all = game_all(inputs)
 let repl_game_now = game_now
@@ -205,7 +208,7 @@ let repl_game_now = game_now
 
 (*REPL LOOP*)
 
-let rec repl (player : player) (rooms : room list) (game_now : game_now) (game_all : game_all) : unit =
+let rec repl (player : player) (rooms : room list) (items : item list) (game_now : game_now) (game_all : game_all) : unit =
   if (game_now.your_points = game_all.all_points) then
     print_string("You win!")
   else
@@ -217,41 +220,49 @@ let rec repl (player : player) (rooms : room list) (game_now : game_now) (game_a
       match h with
         |"inv" ->
         let () = List.iter (Printf.printf "%s \n") (repl_player.player_items) in
-        repl player rooms game_now game_all
-        |"inventory" ->
-        List.iter (Printf.printf "%s \n") (repl_player.player_items) in
-        repl player rooms game_now game_all
+        repl player rooms items game_now game_all
+        |"inventory" -> let () = List.iter (Printf.printf "%s \n") (player.player_items) in
+        repl player rooms items game_now game_all
         | "score" ->
         let () = print_int game_now.your_points in
-        repl player rooms game_now game_all
+        repl player rooms items game_now game_all
         | "turns" ->
         let () = print_int game_now.your_turns in
-        repl player rooms game_now game_all
+        repl player rooms items game_now game_all
         | "quit" ->
         let () = print_string("Quitting the game...\n") in
         exit 0
         | "look" ->
-        let () = print_endline (string_to_room repl_player.player_room (repl_rooms)).room_description in
-        repl player rooms game_now game_all
+        let () = print_endline (string_to_room player.player_room (rooms)).room_description in
+        repl player rooms items game_now game_all
         | _ ->
-        let () = print_string("Not a real function\n") in
-        repl player rooms game_now game_all
+        let player = go_updateplayer h player rooms in
+            let game_now = go_updategame h player game_now rooms in
+            repl player rooms items game_now game_all
       end
     | [h;t] -> begin
+        (*If t in list then do this*)
         match h with
-          | "drop" -> begin
-              let () = function_drop t
-          | "take" -> begin
-            match t with
-            (*how to match on diff items everytime*)
-          end
-          | "take" -> adfdfsasd
-        match t with
-          |
+          | "take" ->
+              let player = takeitem_updateplayer player t in
+              let rooms = takeitem_updaterooms player.player_room t rooms in
+              let game_now = takeitem_updategame game_now t items in
+              repl player rooms items game_now game_all
+          | "drop" ->
+              let player = dropitem_updateplayer player t in
+              let rooms = dropitem_updaterooms player.player_room t rooms in
+              repl player rooms items game_now game_all
+          | "move" ->
+            let player = go_updateplayer t player rooms in
+            let game_now = go_updategame t player game_now rooms in
+            repl player rooms items game_now game_all
+          | _ ->
+          let () = print_string("Not a real function\n") in
+          repl player rooms items game_now game_all
+        end
+    | _ -> let () = print_string("Not a real function\n") in
+    repl player rooms items game_now game_all
 
 
-
-
-
-let () = repl repl_player repl_rooms repl_game_now repl_game_all
+let () = repl repl_player repl_rooms repl_items repl_game_now repl_game_all
 
